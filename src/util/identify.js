1b4fd3e9cbf0b7953198f579ecf162438e5cdbf3
Oliver <oliver.ross@dwp.gsi.gov.uk>
2017-08-02 12:01:21 +0100

    util/identify to get id from title or id

---
+++ src/util/identify.js
@@ -0,0 +1,22 @@
+const graylog = require('./graylog'),
+  finder = require('object-finder');
+
+function get(data, identifier) {
+  let found = finder({ id: identifier }, data);
+  if(found.length === 0) { found = finder({ title: identifier}, data); }
+  return (found.length === 0) ? null : found[0];
+}
+
+module.exports = (dashboard, widget) => {
+  let foundDashboard, foundWidget;
+  return new Promise((resolve, reject) => {
+    graylog.dashboards().then((dashboards) => {
+      foundDashboard = get(dashboards, dashboard);
+      if(widget && foundDashboard) { foundWidget = get(foundDashboard.widgets, widget); }
+      resolve({
+        dashboard: (foundDashboard) ? foundDashboard.id : null,
+        widget: (foundWidget) ? foundWidget.id : null
+      });
+    }).catch(reject);
+  });
+};
---
+++ test/util/identify.test.js
@@ -0,0 +1,123 @@
+let target;
+
+const mockDashboardResolution = [
+  {
+    title: 'mockDashboardTitle_1',
+    id: 'mockDashboardId_1',
+    widgets: [
+      { title: 'mockDashboardWidgetTitle_1_1', id: 'mockDashboardWidgetId_1_1' },
+      { title: 'mockDashboardWidgetTitle_1_2', id: 'mockDashboardWidgetId_1_2' }
+    ]
+  },
+  {
+    title: 'mockDashboardTitle_2',
+    id: 'mockDashboardId_2',
+    widgets: [
+      { title: 'mockDashboardWidgetTitle_2_1', id: 'mockDashboardWidgetId_2_1' },
+      { title: 'mockDashboardWidgetTitle_2_2', id: 'mockDashboardWidgetId_2_2' }
+    ]
+  }
+];
+const mockDashboardError = new Error('mockDashboardError');
+let mockDashboardRejects = false;
+
+const mockDashboards = jest.fn().mockImplementation(() => new Promise((resolve, reject) => {
+  (mockDashboardRejects) ? reject(mockDashboardError) : resolve(mockDashboardResolution);
+}));
+
+const assertions = [
+  {
+    description: 'dashboard id returns the id',
+    args: ['mockDashboardId_1'],
+    response: {
+      dashboard: 'mockDashboardId_1',
+      widget: null
+    }
+  },
+  {
+    description: 'dashboard title returns the id',
+    args: ['mockDashboardTitle_1'],
+    response: {
+      dashboard: 'mockDashboardId_1',
+      widget: null
+    }
+  },
+  {
+    description: 'widget id, dashboard id returns dashboard id, widget id',
+    args: ['mockDashboardId_2', 'mockDashboardWidgetId_2_1'],
+    response: {
+      dashboard: 'mockDashboardId_2',
+      widget: 'mockDashboardWidgetId_2_1'
+    }
+  },
+  {
+    description: 'widget id, dashboard title returns dashboard id, widget id',
+    args: ['mockDashboardTitle_2', 'mockDashboardWidgetId_2_2'],
+    response: {
+      dashboard: 'mockDashboardId_2',
+      widget: 'mockDashboardWidgetId_2_2'
+    }
+  },
+  {
+    description: 'widget title, dashboard id returns dashboard id, widget id',
+    args: ['mockDashboardId_2', 'mockDashboardWidgetId_2_1'],
+    response: {
+      dashboard: 'mockDashboardId_2',
+      widget: 'mockDashboardWidgetId_2_1'
+    }
+  },
+  {
+    description: 'widget title, dashboard title returns dashboard id, widget id',
+    args: ['mockDashboardTitle_1', 'mockDashboardWidgetId_1_2'],
+    response: {
+      dashboard: 'mockDashboardId_1',
+      widget: 'mockDashboardWidgetId_1_2'
+    }
+  }
+];
+
+beforeAll(() => {
+  jest.mock('../../src/util/graylog', () => ({ dashboards: mockDashboards }));
+  target = require('../../src/util/identify');
+});
+
+beforeEach(() => {
+  mockDashboards.mockClear();
+});
+
+test('exports a function', () => {
+  expect(target).toBeDefined();
+  expect(target).toBeInstanceOf(Function);
+});
+
+test('returns a promise', () => {
+  expect(target('blah')).toBeInstanceOf(Promise);
+});
+
+test('gets dashboard data from graylog module', (done) => {
+  target('blah', 'blah').then(() => {
+    expect(mockDashboards).toHaveBeenCalled();
+    done();
+  });
+});
+
+test('reject if dashboard call rejects', (done) => {
+  mockDashboardRejects = true;
+  target('blah', 'blah').catch((err) => {
+    expect(err).toBeInstanceOf(Error);
+    expect(err).toEqual(mockDashboardError);
+    mockDashboardRejects = false;
+    done();
+  });
+});
+
+describe('returns ids when passed id or title', () => {
+  assertions.forEach((assertion) => {
+    test(assertion.description, (done) => {
+      target(...assertion.args).then((res) => {
+        expect(res).toEqual(expect.objectContaining(assertion.response));
+        done();
+      });
+    });
+  });
+});
