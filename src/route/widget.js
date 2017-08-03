1dda0e30da2d9c4a78202d6e706a4f32259191ae
Oliver <oliver.ross@dwp.gsi.gov.uk>
2017-08-03 14:47:22 +0100

    widget router tests

--- src/route/widget.js
+++ src/route/widget.js
@@ -6,8 +6,10 @@ router.route('/:dashboardIdentifier/:widgetIdentifier').get((req, res) => {
   identify(req.params.dashboardIdentifier, req.params.widgetIdentifier).then((id) => {
     if(!id.dashboard) { return res.status(404).send(`no dashboard with identifier "${req.params.dashboardIdentifier}"`); }
     if(!id.widget) { return res.status(404).send(`no widget with identifier "${req.params.widgetIdentifier}"`); }
-    graylog.widget(id).then((value) => {
+    graylog.widgets(id).then((value) => {
       res.json(value);
+    }).catch((err) => {
+      res.status(500).send(err.message);
     });
   });
 });
--- test/route/widget.test.js
+++ test/route/widget.test.js
@@ -1,7 +1,7 @@
 const express = require('express'),
   request = require('supertest');
 
-let mockIdentifyResolution = { mock: 'thing' };
+let mockIdentifyResolution = { dashboard: 'mockDashboardId', widget: 'mockWidgetId' };
 let mockIdentifyError = new Error('mockIdentifyError');
 let mockIdentifyRejects = false;
 
@@ -9,10 +9,18 @@ const mockIdentify = jest.fn().mockImplementation(() => new Promise((resolve, re
   mockIdentifyRejects ? reject(mockIdentifyError) : resolve(mockIdentifyResolution);
 }));
 
+let mockWidgetsRejects = false;
+const mockWidgetsError = new Error('mockWidgetsError');
+let mockWidgetsResolution = { id: 'mockWidgetsResolution' };
+const mockWidgets = jest.fn().mockImplementation(() => new Promise((resolve, reject) => {
+  (mockWidgetsRejects) ? reject(mockWidgetsError) : resolve(mockWidgetsResolution);
+}));
+
 let target, app;
 
 beforeAll(() => {
   jest.mock('../../src/util/identify', () => mockIdentify);
+  jest.mock('../../src/util/graylog', () => ({ widgets: mockWidgets }));
   target = require('../../src/route/widget');
   app = express();
   app.use('/', target);
@@ -25,6 +33,30 @@ beforeEach(() => {
 test('requests identity for url params', (done) => {
   request(app).get('/dashboardIdentifier/widgetIdentifier').then(() => {
     expect(mockIdentify).toHaveBeenCalledWith('dashboardIdentifier', 'widgetIdentifier');
+    done();
+  });
+});
+
+test('calls graylog::widgets with identity', (done) => {
+  request(app).get('/dashboardIdentifier/widgetIdentifier').then(() => {
+    expect(mockWidgets).toHaveBeenCalledWith(mockIdentifyResolution);
+    done();
+  });
+});
+
+test('response is response form graylog::widgets', (done) => {
+  request(app).get('/dashboardIdentifier/widgetIdentifier').then((response) => {
+    expect(response.text).toEqual(JSON.stringify({ id: 'mockWidgetsResolution' }));
+    done();
+  });
+});
+
+test('500 if graylog::widgets rejects', (done) => {
+  mockWidgetsRejects = true;
+  request(app).get('/dashboardIdentifier/widgetIdentifier').then((response) => {
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('mockWidgetsError');
+    mockWidgetsRejects = false;
     done();
   });
 });
